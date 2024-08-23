48e82497397eebbd096a3f344f05b20541151d0d
123110431 JOAO VICTOR DA SILVA LIMEIRA <joao.victor.silva.limeira@lcc2-32.lcc.ufcg.edu.br>
2024-08-23 10:16:48 -0300

    subindo stack do lcc

--- roteiro07/R07-01-Rot-TAD-Linear-environment/src/main/java/adt/stack/StackImpl.java
+++ roteiro07/R07-01-Rot-TAD-Linear-environment/src/main/java/adt/stack/StackImpl.java
@@ -12,33 +12,46 @@ public class StackImpl<T> implements Stack<T> {
 	}
 
 	@Override
-	public T top() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+	public T top(){
+		if (this.isEmpty()){
+			return null;
+		}
+		else{
+			return this.array[this.top];
+		}
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return this.top == -1;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return this.top == array.length - 1;
 	}
 
 	@Override
 	public void push(T element) throws StackOverflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.isFull()){
+			throw new StackOverflowException();
+		}
+		else{
+			this.top ++;
+			this.array[top] = element;
+		}
 	}
 
 	@Override
 	public T pop() throws StackUnderflowException {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (this.isEmpty()){
+			throw new StackUnderflowException();
+		}
+		else{
+			T temp = this.array[top];
+			this.top --;
+			return temp;
+		}
 	}
 
 }
--- roteiro07/R07-01-Rot-TAD-Linear-environment/src/test/java/adt/stack/StudentStackTest.java
+++ roteiro07/R07-01-Rot-TAD-Linear-environment/src/test/java/adt/stack/StudentStackTest.java
@@ -32,8 +32,8 @@ public class StudentStackTest {
 
 	private void getImplementations() {
 		// TODO O aluno deve ajustar aqui para instanciar sua implementação
-		stack1 = null;
-		stack2 = null;
+		stack1 = new StackImpl<Integer>(4);
+		stack2 = new StackImpl<Integer>(2);
 		stack3 = null;
 	}
 
@@ -66,7 +66,9 @@ public class StudentStackTest {
 
 	@Test(expected = StackOverflowException.class)
 	public void testPushComErro() throws StackOverflowException {
-		stack1.push(new Integer(5)); // levanta excecao apenas se o tamanhonao
+		Integer add = 5;
+		stack1.push(add);
+		stack1.push(add); // levanta excecao apenas se o tamanhonao
 										// permitir outra insercao
 	}
 
@@ -82,7 +84,9 @@ public class StudentStackTest {
 
 	@Test(expected = StackUnderflowException.class)
 	public void testPopComErro() throws StackUnderflowException {
-		assertEquals(new Integer(3), stack1.pop()); // levanta excecao apenas se
-													// stack1 for vazia
+		assertEquals(new Integer(3), stack1.pop()); 										// levanta excecao apenas se
+		assertEquals(new Integer(2), stack1.pop());
+		assertEquals(new Integer(1), stack1.pop());
+		stack1.pop();											// stack1 for vazia
 	}
 }
