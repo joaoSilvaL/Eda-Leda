804e63c971250f4afe50181d69b1c37663281652
123110431 JOAO VICTOR DA SILVA LIMEIRA <joao.victor.silva.limeira@lcc2-32.lcc.ufcg.edu.br>
2024-08-23 10:48:40 -0300

    Queue normal implementadada

--- roteiro07/R07-01-Rot-TAD-Linear-environment/src/test/java/adt/queue/StudentQueueTest.java
+++ roteiro07/R07-01-Rot-TAD-Linear-environment/src/test/java/adt/queue/StudentQueueTest.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class StudentQueueTest {
 
-	public Queue<Integer> queue1;
+	public QueueImpl<Integer> queue1;
 	public Queue<Integer> queue2;
 	public Queue<Integer> queue3;
 
@@ -32,9 +33,9 @@ public class StudentQueueTest {
 
 	private void getImplementations() {
 		// TODO O aluno deve ajustar aqui para instanciar sua implementação
-		queue1 = null;
-		queue2 = null;
-		queue3 = null;
+		queue1 = new QueueImpl<>(4);
+		queue2 = new QueueImpl<>(2);
+		queue3 = new QueueImpl<>(3);
 	}
 
 	// MÉTODOS DE TESTE
@@ -66,7 +67,8 @@ public class StudentQueueTest {
 
 	@Test(expected = QueueOverflowException.class)
 	public void testEnqueueComErro() throws QueueOverflowException {
-		queue1.enqueue(new Integer(5)); // vai depender do tamanho que a fila
+		queue1.enqueue(new Integer(5));
+		queue1.enqueue(new Integer(2));// vai depender do tamanho que a fila
 										// foi iniciada!!!
 	}
 
@@ -82,8 +84,11 @@ public class StudentQueueTest {
 
 	@Test(expected = QueueUnderflowException.class)
 	public void testDequeueComErro() throws QueueUnderflowException {
-		assertEquals(new Integer(1), queue1.dequeue()); // vai depender do
-														// tamanho que a fial
+		assertEquals(new Integer(1), queue1.dequeue());
+		while (!this.queue1.isEmpty()) {
+			this.queue1.dequeue();
+		} // vai depender do
+		this.queue1.dequeue();						// tamanho que a fial
 														// foi iniciada!!!
 	}
 }
